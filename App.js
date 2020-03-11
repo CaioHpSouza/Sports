1dfd40335e699fdb3f34b890677b001e6865d3e1
CaioHpSouza <caiosouza9619@gmail.com>
2020-03-10 23:21:41 -0300

    Iniciando testes

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Agora ta funfando tudo</Text>
+      <Text>Obrigado meu deus </Text>
+      <Text>Teste 05/03 </Text>
+      
+      
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
